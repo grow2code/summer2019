9f9939efa65c0403889a317f30f72f98aadd2911
jiham7 <jiham7@gmail.com>
2019-07-15 11:07:38 -0500

    Session 4

--- Sessions/Session 3 (6:29)/javascript.js
+++ Sessions/Session 3 (6:29)/javascript.js
@@ -1,6 +1,11 @@
 var question = prompt("What is your name?");
-var question2 prompt("What is your age?");
+var question2 = prompt("What is your age?");
 
 var newName = question + "Bungaloo";
 
 alert("Your new name is " + newName + "!!!!");
+
+
+function f1() {
+  console.log("Hello world");
+}
---
+++ Sessions/Session 4/practice.js
@@ -0,0 +1,17 @@
+var a = 900;
+
+if(a > 5 ) {
+  alert("a is greater than 5");
+}
+if(a == 50) {
+  alert("a is 50");
+}
+else {
+  alert("a is less than or equal to 5");
+}
+
+var x = 0;
+while(x < 10) {
+  alert(x);
+  x = x + 1;
+}
