a30448a05398a35da69e5e426d721abbe87400d8
jiham7 <jiham7@gmail.com>
2019-07-23 11:11:20 -0500

    Session 5

---
+++ Sessions/Session 5/practice.js
@@ -0,0 +1,36 @@
+//
+// var x = 0;
+//
+// while(x < 10) {
+//   if(x%4 == 0) {
+//
+//   }
+//   else {
+//     alert(x);
+//   }
+//   x = x + 1;
+//
+// }
+
+//
+// for(i = 0; i < 100; i = i + 1) {
+//   if(i%5 != 0) {
+//     alert(i);
+//   }
+// }
+
+var h = document.querySelector("h1");
+
+h.style.color = 'red';
+
+
+function getRandomColor() {
+  var color1 = '#FFFFFF';
+  var color2 = '#FFFFFF';
+  var color3 = '#FFFFFF';
+
+  for(i=0; i < 3; i++) {
+    
+  }
+
+}
